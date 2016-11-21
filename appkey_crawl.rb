24f125c02253ce92f2a8fe6e34fb5c71a394cf71
Billy Kovalsky <billy@yotpo.com>
2016-11-21 15:58:43 +0200

    added support for urls arguments

--- appkey_crawl.rb
+++ appkey_crawl.rb
@@ -1,4 +1,6 @@
-require 'anemone'
+require 'open-uri'
+require 'open_uri_redirections'
+
 app_keys = {}
 
 if ARGV.empty?
@@ -7,24 +9,19 @@ if ARGV.empty?
 end
 urls = ARGV
 
+
 print "working"
-Anemone.crawl(urls) do |anemone|
+
+
+urls.each do |url|
   print "."
-  anemone.focus_crawl do |page|
-
-    anemone.focus_crawl { |page| page.links.slice(0..5) }
-
-    if page.body.include?('.yotpo.com/')
-      app_key = page.body.split('.yotpo.com/')[1].split('/widget.js')[0]
-      app_keys[page.url] = app_key
-    else
-      app_keys[page.url] = "can't find yotpo appkey"
-    end
-
-    page.url
-
+  html = open(url,{ :allow_redirections => :safe, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36' }).read
+  if html.include?('.yotpo.com/')
+    app_key = html.split('.yotpo.com/')[1].split('/widget.js')[0]
+    app_keys[url] = app_key
+  else
+    app_keys[url] = "can't find yotpo appkey"
   end
-
 end
 
 puts "\n"
