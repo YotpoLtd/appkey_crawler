337ce38f23b0ea6d55840003217ef2650b47fc69
Billy Kovalsky <billy@yotpo.com>
2018-01-23 12:28:22 +0200

    feat: catch URL errors

--- appkey_crawl.rb
+++ appkey_crawl.rb
@@ -15,13 +15,20 @@ print "working"
 
 urls.each do |url|
   print "."
-  html = open(url,{ :allow_redirections => :safe, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36' }).read
+  begin
+    html = open(url,{ :allow_redirections => :safe, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36' }).read
+  rescue
+    app_keys[url] = "URL Error"
+    next
+  end
+
   if html.include?('.yotpo.com/')
     app_key = html.split('.yotpo.com/')[1].split('/widget.js')[0]
     app_keys[url] = app_key
   else
     app_keys[url] = "can't find yotpo appkey"
   end
+
 end
 
 puts "\n"
